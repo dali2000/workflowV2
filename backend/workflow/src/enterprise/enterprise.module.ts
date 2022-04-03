d38cc377846bbf02f1079077f1ff4ab138356364
dali2000 <medalikebair@gmail.com>
2022-04-04 00:35:39 +0100

    Enterprise Apis

---
+++ backend/workflow/src/enterprise/enterprise.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnterpriseController } from './enterprise.controller';
+import { Enterprise } from './enterprise.entity';
+import { EnterpriseService } from './enterprise.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Enterprise]),
+  JwtModule.register({
+    secret: 'secret',
+    signOptions: { expiresIn: '1d' },
+  })
+],
+  controllers: [EnterpriseController],
+  exports: [],
+  providers: [EnterpriseService]
+})
+export class EnterpriseModule {}
