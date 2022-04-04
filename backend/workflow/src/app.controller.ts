eaef710350f5288edc9bc5da994ec41f22e730b2
dali2000 <medalikebair@gmail.com>
2022-04-04 12:32:49 +0100

    flink update profil

--- backend/workflow/src/app.controller.ts
+++ backend/workflow/src/app.controller.ts
@@ -8,8 +8,8 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-  
+  constructor(private readonly appService: AppService) { }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
@@ -17,6 +17,6 @@ export class AppController {
   }
 
 
-  } 
+}
 
 
