e0e24946331ac1e3286198ea3b1c2a642e5353ab
dali2000 <medalikebair@gmail.com>
2022-04-10 15:13:39 +0200

    fix get admins

--- frontend/workflow/src/app/superAdmin/list-admin/list-admin.component.ts
+++ frontend/workflow/src/app/superAdmin/list-admin/list-admin.component.ts
@@ -13,46 +13,39 @@ export class ListAdminComponent implements OnInit {
 
   constructor(private http: HttpClient, private router: Router) { }
   term: any;
- 
+
   admins:any;
   data:any
   token:any
   user:any
-
+  role="admin"
   list :any=[];
   j = 0;
   ngOnInit(): void {
+    this.list = []
     this.token = localStorage.getItem('token');
     this.data = jwtDecode(this.token);
     this.user = this.data.user;
     console.log(this.user);
 
-
-
-
-
     this.getAllAdmin()
   }
 
   getAllAdmin() {
-    this.http.get('http://localhost:3000/user/users').subscribe(res => {
-      this.admins = res;
-      // console.log(res);
-      for(let i=0;i<this.admins.length;i++){
-        if(this.admins[i].role=="admin"){
-          // console.log(this.admins[i])
-          this.list[this.j]=this.admins[i]
-          this.j++
-        }
-      }
-      console.log(this.list)
+
+    this.list = []
+    this.http.get('http://localhost:3000/user/getAdmins/admin').subscribe(res => {
+      console.log(res)
+      this.admins = res
+
     });
 
   }
   deleteAdmin(id:any) {
     this.http.delete('http://localhost:3000/user/deleteUser/' + id).subscribe(res => {
-      this.getAllAdmin();
       console.log(res)
+
+      this.ngOnInit()
     });
 
   }
